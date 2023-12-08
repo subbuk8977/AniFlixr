a2654add0c3f189f01290af4383c34b1407d68a7
Calisto-Mathias <calisto.mathias.25@gmail.com>
2023-12-09 02:35:39 +0530

    Added Responsiveness for the NavBar

--- front-end/src/components/navbar/Navbar.jsx
+++ front-end/src/components/navbar/Navbar.jsx
@@ -3,9 +3,13 @@ import { useState } from "react";
 
 import "./Navbar.scss";
 import { Search } from "react-feather";
+import { Menu } from "react-feather";
+import { X } from "react-feather";
 
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(0);
+  const [clicked, setClicked] = useState(0);
+
   window.onscroll = () => {
     setIsScrolled(window.pageYOffset);
     return () => (window.onscroll = null);
@@ -13,7 +17,9 @@ const Navbar = () => {
   return (
     <nav
       className="navbar"
-      style={{ backgroundColor: `rgb(0,0,0,${isScrolled / 100})` }}
+      style={{
+        backgroundColor: `rgb(0,0,0,${!clicked ? isScrolled / 100 : 1})`,
+      }}
     >
       <div className="left">
         <h1 className="logo">
@@ -34,6 +40,33 @@ const Navbar = () => {
         <Search></Search>
         <button className="login">Log In</button>
       </div>
+      <div
+        className="navigation_mobile"
+        style={{ left: `${!clicked ? "-100vw" : "0vw"}` }}
+      >
+        <span>Home</span>
+        <span>Trending</span>
+        <span>Categories</span>
+        <span>Anime Release</span>
+        <span>About</span>
+        <span>Search</span>
+        <span>Log In </span>
+      </div>
+      {!clicked ? (
+        <Menu
+          className="menu"
+          onClick={() => {
+            setClicked(1);
+          }}
+        ></Menu>
+      ) : (
+        <X
+          className="menu"
+          onClick={() => {
+            setClicked(0);
+          }}
+        ></X>
+      )}
     </nav>
   );
 };
